ae54b9448d78728fdca8ae243052c4951290160c
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-02-11 17:35:54 -0700

    set up user prompts

--- index.js
+++ index.js
@@ -19,6 +19,38 @@ inquirer
 			name: 'email',
 			message: 'What is your email address?',
 		},
+		{
+			type: 'list',
+			name: 'license',
+			message: 'Which license does your project have?',
+			choices: ['MIT', 'Apache', 'GNU'],
+		},
+		{
+			type: 'input',
+			name: 'description',
+			message: 'Write a description for your project:',
+		},
+		{
+			type: 'input',
+			name: 'install',
+			message: 'What command is needed to install dependencies?',
+		},
+		{
+			type: 'input',
+			name: 'test',
+			message: 'What command is needed to run tests?',
+		},
+		{
+			type: 'input',
+			name: 'usage',
+			message: 'Enter any information the user needs for using the repo:',
+		},
+		{
+			type: 'input',
+			name: 'contribution',
+			message:
+				'Enter any information the user needs for contributing to the repo:',
+		},
 	])
 	.then((response) => {
 		console.log(response);
